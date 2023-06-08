c88ae0b42ae48b610d306f4bdd3d6207bb71ecd4
gujrali22 <gujrali22@bonaventure.edu>
2023-06-07 21:54:20 -0500

    cifar 10 with 89% accuracy

--- cifar_10_classification.py
+++ cifar_10_classification.py
@@ -1,7 +1,7 @@
 import numpy as np
 import keras
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, MaxPooling1D
+from keras.layers import Dense, Dropout, MaxPooling2D, Conv2D, Flatten, BatchNormalization
 from sklearn.metrics import confusion_matrix
 import seaborn as sns
 from keras.datasets import cifar10
@@ -30,26 +30,42 @@ y_test = keras.utils.to_categorical(y_test, num_classes)
 x_train = x_train / 255.0
 x_test = x_test / 255.0
 
-x_train = x_train.reshape(x_train.shape[0], -1)
-x_test = x_test.reshape(x_test.shape[0], -1)
-print(x_train.shape)
-print(y_train.shape)
+#x_train = x_train.reshape(x_train.shape[0], -1)
+#x_test = x_test.reshape(x_test.shape[0], -1)
+#print(x_train.shape)
+#print(y_train.shape)
 
 # Make NN
 
 model = Sequential()
-model.add(Dense(units=512, input_shape=(3072,), activation='relu'))
-keras.layers.MaxPooling1D (
-   pool_size = 3, 
-   strides = None, 
-   padding = 'valid', 
-   data_format = 'channels_last'
-)
-model.add(Dense(units=512, activation='relu'))
-model.add(Dense(units=512, activation='relu'))
-model.add(Dense(units=10, activation='softmax'))
+model.add(Conv2D(32, (3, 3), activation = 'relu', padding = 'same', input_shape = (32, 32, 3)))
+model.add(BatchNormalization())
+model.add(Dropout(0.25))
+model.add(Conv2D(32, (3, 3), activation = 'relu', padding = 'same'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(2, 2))
+
+model.add(Conv2D(64, (3, 3), activation = 'relu', padding = 'same'))
+model.add(BatchNormalization())
+model.add(Dropout(0.25))
+model.add(Conv2D(64, (3, 3), activation = 'relu', padding = 'same'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(2, 2))
+
+model.add(Conv2D(128, (3, 3), activation = 'relu', padding = 'same'))
+model.add(BatchNormalization())
+model.add(Dropout(0.25))
+model.add(Conv2D(128, (3, 3), activation = 'relu', padding = 'same'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(2, 2))
+
+model.add(Flatten())
+model.add(Dense(256, activation = 'relu'))
+model.add(BatchNormalization())
+model.add(Dropout(0.5))
+model.add(Dense(10, activation = 'softmax'))
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 model.summary()
-model.fit(x=x_train, y=y_train, batch_size=512, epochs=10)
+model.fit(x=x_train, y=y_train, batch_size=20, epochs=16)
 test_loss, test_acc = model.evaluate(x_test, y_test)
 print("Test Loss: {}, Test Accuracy: {}".format(test_loss, test_acc))
